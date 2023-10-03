210d5ec4f154fbbe8d5e66a5b8b1f0b0b4d04042
Greg Chambers <DC49P74@tn.gov>
2023-10-03 14:12:35 -0500

    add clean date function

---
+++ R/dates.R
@@ -0,0 +1,15 @@
+
+#' Cleans dates in a variety of formats
+#'
+#' @param x vector of date-like values
+#' @param format string recognized by format() to format the date
+#'
+#' @return Date or formatted string
+#' @export
+tn_clean_date <- function(x, format = "%m/%d/%Y") {
+  x <- as.character(x)
+  x[grepl("^\\d{5}$", x)] <- as.character(as.Date(as.numeric(x[grepl("^\\d{5}$", x)]),origin = "1899-12-30"))
+  new_dates<- anytime::anydate(x)
+  new_dates[is.na(new_dates)]<-parsedate::parse_date(x[is.na(new_dates)])
+  return(format(new_dates,format))
+}
