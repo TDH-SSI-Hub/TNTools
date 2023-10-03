edf70da3acea738deadadb397252f1d4130a1527
Greg Chambers <DC49P74@tn.gov>
2023-10-03 14:53:26 -0500

    changed default format for tn_clean_date

--- R/dates.R
+++ R/dates.R
@@ -6,10 +6,14 @@
 #'
 #' @return Date or formatted string
 #' @export
-tn_clean_date <- function(x, format = "%m/%d/%Y") {
+tn_clean_date <- function(x, format = NA) {
   x <- as.character(x)
   x[grepl("^\\d{5}$|^\\d{5}\\..*$", x)] <- as.character(as.Date(as.numeric(x[grepl("^\\d{5}", x)]),origin = "1899-12-30"))
   new_dates<- anytime::anydate(x)
   new_dates[is.na(new_dates)]<-parsedate::parse_date(x[is.na(new_dates)])
+  if(!is.na(format)){
   return(format(new_dates,format))
+  }else{
+    return(new_dates)
+  }
 }
