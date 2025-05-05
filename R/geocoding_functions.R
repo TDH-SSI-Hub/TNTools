104e6a858c964ce18ff19fe4f57d0e44f3f019f2
Greg Chambers <greg.chambers@tn.gov>
2025-05-05 09:11:53 -0500

    Main changes to outsource geocoding

---
+++ R/geocoding_functions.R
@@ -0,0 +1,28 @@
+
+#' @importFrom TNgeocodeR tn_county_to_region
+#' @export
+TNgeocodeR::tn_county_to_region
+
+#' @importFrom TNgeocodeR tn_geocode_vector
+#' @export
+TNgeocodeR::tn_geocode_vector
+
+#' @importFrom TNgeocodeR tn_geocode_addresses
+#' @export
+TNgeocodeR::tn_geocode_addresses
+
+#' @importFrom TNgeocodeR tn_api_outputs
+#' @export
+TNgeocodeR::tn_api_outputs
+
+#' @importFrom TNgeocodeR tn_api_inputs
+#' @export
+TNgeocodeR::tn_api_inputs
+
+#' @importFrom TNgeocodeR tn_geocoder_specialcases
+#' @export
+TNgeocodeR::tn_geocoder_specialcases
+
+#' @importFrom TNgeocodeR tn_geocoder_url
+#' @export
+TNgeocodeR::tn_geocoder_url
